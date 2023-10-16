49ffb93db3c11da741a24960e489db7242035f74
Afroz pathan <afroz.p8111@gmail.com>
2023-10-16 12:20:41 +0530

    completed

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled/screens/home_screen.dart';
 import 'package:untitled/utils/colors.dart';
 
@@ -18,11 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 3), () {
-      const Center(
-        child: SpinKitCircle(
-          color: roseWood,
-        ),
-      );
+
 
       Navigator.pushReplacement(
         context,
@@ -31,21 +28,31 @@ class _SplashScreenState extends State<SplashScreen> {
         ),
       );
 
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Image.asset(
-            "assets/splash_pic.jpg",
-            height: double.infinity,
-            width: double.infinity,
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
-          ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/splash_pic.jpg",
+              fit: BoxFit.cover,
+              height:  height * .8,
+            ),
+            SizedBox(height: height * 0.04,),
+            Text("TOP HEADLINES" , style: GoogleFonts.anton(letterSpacing: .6 , color: Colors.grey.shade700),),
+            SizedBox(height: height * 0.04,),
+            const SpinKitCircle(
+              color: roseWood ,
+              size: 40,
+            )
+
+          ],
         ),
       ),
     );
